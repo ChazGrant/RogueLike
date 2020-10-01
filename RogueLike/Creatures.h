64e997e41b848166d2e36c66035fc180046f6fa1
ChazGrant <ChazGrant@users.noreply.github.com>
2020-10-01 18:31:17 +0300

    Added new class

--- RogueLike/Creatures.h
+++ RogueLike/Creatures.h
@@ -1,12 +1,17 @@
 #pragma once
+#include <iostream>
 class Creature
 {
 protected:
 	int attackPoints, healthPoints, experiencePoints;
+	char name[20];
 public:
 	virtual void Set_attackPoints(int a) = 0;
 	virtual void Set_healthPoints(int h) = 0;
 	virtual void Set_experiencePoints(int h) = 0;
+	virtual void receiveDeath() = 0;
+
+	virtual void printInfo(char message[], int delay) = 0;
 
 	virtual int Get_attackPoints() = 0;
 	virtual int Get_healthPoints() = 0;
--- RogueLike/RogueLike.cpp
+++ RogueLike/RogueLike.cpp
@@ -4,15 +4,58 @@
 #include "stdafx.h"
 #include <iostream>
 #include "Creatures.cpp"
+#include "windows.h"
 
 using namespace std;
 
-int main()
+int isDead(Creature *c)
 {
-	Creature *s = new Sorcerer;
-
-	cout << s->Get_experiencePoints() << endl;
-
-    return 0;
+	return !c->Get_healthPoints();
 }
 
+int main()
+{
+	SetConsoleTitle(_T("RogueLike by ME"));
+	setlocale(LC_ALL, "rus");
+	int choice;
+	char name[25];
+	cout << "Выберите ваш класс(1 - Маг, 2 - Некромант): ";
+	cin >> choice;
+	cout << "Выберите имя вашего героя: ";
+	cin >> name;
+	switch (choice)
+	{
+	case 1:
+	{
+		Sorcerer *s = new Sorcerer(name);
+		break;
+	}
+
+	case 2:
+	{
+		Necromancer *n = new Necromancer(name);
+		break;
+	}
+
+	}
+	/*Sorcerer *s = new Sorcerer("Dumbledore");
+	printf("%d\n", s->Get_attackPoints());
+	delete s;
+	Necromancer *n = new Necromancer("Guldan");
+	cout << n->Get_healthPoints() << endl;
+	n->receiveDeath();
+	cout << n->Get_healthPoints() << endl;
+	n->receiveDeath();
+	n->receiveDeath();
+	n->Set_healthPoints(0);
+	isDead(n) ? delete n : printf("NOT YET");
+	try
+	{
+		n->Get_attackPoints();
+	}
+	catch (...)
+	{
+		cout << "ERROR\n";
+	}*/
+    return 0;
+}
